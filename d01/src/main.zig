c19ab0aeb971410344f6136b116f94127559b874
Rory Self <rory.self@protonmail.com>
2025-12-10 17:40:11 +1100

    Cleaned up day 1 implementation

--- d01/src/main.zig
+++ d01/src/main.zig
@@ -26,37 +26,6 @@ const Direction = enum {
 const Instruction = struct {
     direction: Direction,
     num_turns: u16,
-
-    fn allocFromFile(file_path: []const u8, allocator: std.mem.Allocator) !InstructionList {
-        const instructions_file = try std.fs.cwd().openFile(file_path, .{ .mode = .read_only });
-        defer instructions_file.close();
-
-        var reader_buf: [BUF_SIZE]u8 = undefined;
-        var file_reader = instructions_file.reader(&reader_buf);
-        var reader = &file_reader.interface;
-
-        var instructions: InstructionList = .empty;
-        while (true) {
-            const direction_char: u8 = reader.takeByte() catch |err| {
-                if (err == error.EndOfStream) {
-                    break;
-                }
-
-                return err;
-            };
-
-            const direction = try Direction.fromChar(direction_char);
-
-            const num_turns_str: []u8 = try reader.takeDelimiterExclusive('\n');
-            const num_turns: u16 = try std.fmt.parseInt(u16, num_turns_str, 10);
-            reader.toss(1);
-
-            const instruction = Instruction{ .direction = direction, .num_turns = num_turns };
-            try instructions.append(allocator, instruction);
-        }
-
-        return instructions;
-    }
 };
 const InstructionList = std.ArrayList(Instruction);
 
@@ -65,13 +34,12 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var instructions: InstructionList = try Instruction.allocFromFile(FILE_PATH, allocator);
-    defer instructions.deinit(allocator);
+    const instructions = try instructionsFromFile(FILE_PATH, allocator);
 
     var curr_num: u8 = STARTING_NUMBER;
     var zero_clicks: u16 = 0;
     var num_zeroes: u16 = 0;
-    for (try instructions.toOwnedSlice(allocator)) |instruction| {
+    for (instructions) |instruction| {
         const dial_result = turn_dial(curr_num, instruction);
         curr_num = dial_result[0];
         zero_clicks += dial_result[1];
@@ -89,6 +57,33 @@ pub fn main() !void {
     try stdout_writer.flush();
 }
 
+fn instructionsFromFile(file_path: []const u8, allocator: std.mem.Allocator) ![]const Instruction {
+    const instructions_file = try std.fs.cwd().openFile(file_path, .{ .mode = .read_only });
+    defer instructions_file.close();
+
+    var reader_buf: [BUF_SIZE]u8 = undefined;
+    var file_reader = instructions_file.reader(&reader_buf);
+    var reader = &file_reader.interface;
+
+    var instructions: InstructionList = .empty;
+    while (reader.takeByte()) |direction_char| {
+        const direction = try Direction.fromChar(direction_char);
+
+        const num_turns_str: []u8 = try reader.takeDelimiterExclusive('\n');
+        const num_turns: u16 = try std.fmt.parseInt(u16, num_turns_str, 10);
+        reader.toss(1);
+
+        const instruction = Instruction{ .direction = direction, .num_turns = num_turns };
+        try instructions.append(allocator, instruction);
+    } else |err| {
+        if (err != error.EndOfStream) {
+            return err;
+        }
+    }
+
+    return try instructions.toOwnedSlice(allocator);
+}
+
 fn turn_dial(curr_num: u8, instruction: Instruction) struct { u8, u8 } {
     const num_turns: u16 = instruction.num_turns;
     if (num_turns == 0) {
@@ -96,25 +91,25 @@ fn turn_dial(curr_num: u8, instruction: Instruction) struct { u8, u8 } {
     }
 
     // Calculate full rotations and remaining turns
-    const effective_turns: u8 = @truncate(num_turns % NUM_DIALS);
-    var zero_clicks: u8 = @truncate(@divFloor(num_turns, NUM_DIALS));
+    const effective_turns: u8 = @intCast(num_turns % NUM_DIALS);
+    var zero_clicks: u8 = @intCast(@divFloor(num_turns, NUM_DIALS));
     if (effective_turns == 0) {
         return .{ curr_num, zero_clicks };
     }
 
-    const new_num: u8 = if (instruction.direction == .left) left: {
+    const new_num: u8 = if (instruction.direction == .left) left_turn: {
         if (curr_num <= effective_turns and curr_num != 0) {
             zero_clicks += 1;
         }
 
-        break :left @truncate((curr_num + NUM_DIALS - effective_turns) % NUM_DIALS);
-    } else right: {
+        break :left_turn (curr_num + NUM_DIALS - effective_turns) % NUM_DIALS;
+    } else right_turn: {
         const raw_turn_total: u8 = curr_num + effective_turns;
         if (raw_turn_total >= NUM_DIALS and curr_num != 0) {
             zero_clicks += 1;
         }
 
-        break :right @truncate(raw_turn_total % NUM_DIALS);
+        break :right_turn raw_turn_total % NUM_DIALS;
     };
 
     return .{ new_num, zero_clicks };
