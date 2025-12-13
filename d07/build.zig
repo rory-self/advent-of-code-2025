214be7cf82952c7c028cb848bed0057695fb1635
Rory Self <rory.self@protonmail.com>
2025-12-13 18:10:35 +1100

    Cleaned up build script

--- d07/build.zig
+++ d07/build.zig
@@ -4,28 +4,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = .Debug;
 
-    const utils = b.addModule("utils", .{
+    const utils_module = b.addModule("utils", .{
         .root_source_file = b.path("../utils.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const main_file_path = b.path("src/main.zig");
-    const exe = b.addExecutable(.{ .name = "d07", .root_module = b.createModule(.{
-        .root_source_file = main_file_path,
+    const main_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{.{ .name = "utils", .module = utils }},
-    }), .use_llvm = true });
+        .imports = &.{.{ .name = "utils", .module = utils_module }},
+    });
+    const exe = b.addExecutable(.{
+        .name = "d07",
+        .root_module = main_module,
+        .use_llvm = true,
+    });
 
     const test_step = b.step("test", "Run unit tests");
-    const tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = main_file_path,
-            .target = target,
-            .imports = &.{.{ .name = "utils", .module = utils }},
-        }),
-    });
+    const tests = b.addTest(.{ .root_module = main_module });
     const run_tests = b.addRunArtifact(tests);
     test_step.dependOn(&run_tests.step);
 
