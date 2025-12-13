105b48b571e10b3889edcfa5693b3b681105af02
Rory Self <rory.self@protonmail.com>
2025-12-13 21:00:52 +1100

    Added type aliases for clarity

--- d07/src/main.zig
+++ d07/src/main.zig
@@ -7,7 +7,13 @@ const SPLITTER_CHARACTER = '^';
 const IO_BUF_SIZE = 128;
 
 const Allocator = std.mem.Allocator;
-const BeamMap = std.AutoArrayHashMap(usize, u64);
+const NumTimelinesType = u64;
+const NumSplitsType = u16;
+const BeamSimulationResult = struct {
+    num_splits: NumSplitsType,
+    num_timelines: NumTimelinesType,
+};
+const BeamMap = std.AutoArrayHashMap(usize, NumTimelinesType);
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -15,12 +21,15 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     const input_filepath = try utils.getFilepathArg(allocator);
-    const beam_splits, const timelines = try simulateBeamSplits(input_filepath, allocator);
 
-    try utils.printToStdout("{d} beam splits. {d} timelines\n", .{ beam_splits, timelines });
+    const sim_result = try simulateBeamSplits(input_filepath, allocator);
+    const num_splits = sim_result.num_splits;
+    const num_timelines = sim_result.num_timelines; 
+
+    try utils.printToStdout("{d} beam splits. {d} timelines\n", .{ num_splits, num_timelines });
 }
 
-fn simulateBeamSplits(input_filepath: []const u8, allocator: Allocator) !struct { u16, u64 } {
+fn simulateBeamSplits(input_filepath: []const u8, allocator: Allocator) !BeamSimulationResult {
     const file_contents = try utils.readAllFromFile(input_filepath, allocator);
 
     var line_it = std.mem.tokenizeScalar(u8, file_contents, '\n');
@@ -29,10 +38,10 @@ fn simulateBeamSplits(input_filepath: []const u8, allocator: Allocator) !struct 
     var beam_map: BeamMap = .init(allocator);
     try beam_map.putNoClobber(start_pos, 1);
 
-    var beam_splits: u16 = 0;
+    var num_splits: NumSplitsType = 0;
     while (line_it.next()) |line| {
         const curr_beam_positions = try allocator.dupe(usize, beam_map.keys());
-        const incident_timelines = try allocator.dupe(u64, beam_map.values());
+        const incident_timelines = try allocator.dupe(NumTimelinesType, beam_map.values());
 
         for (curr_beam_positions, incident_timelines) |beam_pos, prev_timelines| {
             if (line[beam_pos] != SPLITTER_CHARACTER) {
@@ -44,16 +53,16 @@ fn simulateBeamSplits(input_filepath: []const u8, allocator: Allocator) !struct 
             if (!beam_map.swapRemove(beam_pos)) {
                 return error.ArrayHashMapFail;
             }
-            beam_splits += 1;
+            num_splits += 1;
         }
     }
 
-    var timelines: u64 = 0;
+    var num_timelines: NumTimelinesType = 0;
     for (beam_map.values()) |beam_timelines| {
-        timelines += beam_timelines;
+        num_timelines += beam_timelines;
     }
 
-    return .{ beam_splits, timelines };
+    return .{ .num_splits = num_splits, .num_timelines = num_timelines };
 }
 
 fn findStartPos(line_it: *std.mem.TokenIterator(u8, std.mem.DelimiterType.scalar)) ?usize {
@@ -66,7 +75,7 @@ fn findStartPos(line_it: *std.mem.TokenIterator(u8, std.mem.DelimiterType.scalar
    return null;
 }
 
-fn beamMapInsert(beam_map: *BeamMap, beam_pos: usize, new_timelines: u64) !void {
+fn beamMapInsert(beam_map: *BeamMap, beam_pos: usize, new_timelines: NumTimelinesType) !void {
     if (beam_map.get(beam_pos)) |curr_timelines| {
         try beam_map.put(beam_pos, new_timelines + curr_timelines);
         return;
@@ -80,7 +89,7 @@ test "Example" {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const beam_splits, const timelines = try simulateBeamSplits(TEST_INPUT_PATH, allocator);
-    try std.testing.expect(beam_splits == 21);
-    try std.testing.expect(timelines == 40);
+    const sim_result = try simulateBeamSplits(TEST_INPUT_PATH, allocator);
+    try std.testing.expect(sim_result.num_splits == 21);
+    try std.testing.expect(sim_result.num_timelines == 40);
 }
